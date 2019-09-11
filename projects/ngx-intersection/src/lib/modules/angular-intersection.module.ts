a8fa282f0f71f48dea0b426940ab1e4158291696
Alexander Nickel <mr.alexander.nickel@gmail.com>
2019-09-11 22:36:58 +0200

    Import NgModule instead of exporting with different name

--- projects/ngx-intersection/src/lib/modules/angular-intersection.module.ts
+++ projects/ngx-intersection/src/lib/modules/angular-intersection.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { directivesAlias } from './intersection-directives-alias.module';
+import { IntersectionDirectivesAliasModule } from './intersection-directives-alias.module';
 /**
  * This module is here only for legacy-reasons, but since it doesn't hold *any*
  * own logic we can easily keep on releasing it.
  */
 @NgModule({
-  declarations: directivesAlias,
-  exports: directivesAlias
+  imports: [IntersectionDirectivesAliasModule]
 })
 export class AngularIntersectionModule {}
