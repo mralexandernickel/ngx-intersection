ab38f9ddd9667517a933e2706f977fb8a819a794
Alexander Nickel <mr.alexander.nickel@gmail.com>
2022-01-05 13:37:22 +0100

    chore: Upgrade to angular 11

--- src/app/components/scroll-motivation/scroll-motivation.component.spec.ts
+++ src/app/components/scroll-motivation/scroll-motivation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ScrollMotivationComponent } from './scroll-motivation.component';
 
@@ -6,7 +6,7 @@ describe('ScrollMotivationComponent', () => {
   let component: ScrollMotivationComponent;
   let fixture: ComponentFixture<ScrollMotivationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ScrollMotivationComponent ]
     })
