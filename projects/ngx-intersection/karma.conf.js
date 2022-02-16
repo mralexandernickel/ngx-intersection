7ae14ed6af6ee2290ccb5ad9cb791ff24a4685a1
Alexander Nickel <mr.alexander.nickel@gmail.com>
2022-02-16 17:35:39 +0100

    chore: Generate correct outputs durign testing

--- projects/ngx-intersection/karma.conf.js
+++ projects/ngx-intersection/karma.conf.js
@@ -15,17 +15,13 @@ module.exports = function(config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-intersection'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/ngx-intersection'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
       ]
     },
     reporters: ['progress', 'kjhtml'],
